0ce6f6ae01e43f370e66241742bcbd0d7014890d
Alexander <alexander.arntzen@gmail.com>
2020-09-29 13:21:52 +0200

    Ferdig med oppgave 1

--- part1/quadrature.py
+++ part1/quadrature.py
@@ -12,14 +12,34 @@ quadrature_table_1D = {
         [+ np.sqrt((3 + 2 * np.sqrt(6 / 5)) / 7), (18 - np.sqrt(30)) / 36]]
 }
 def quadrature1D(a, b, Nq, g):
-    integral = 0
+    I = 0
     for row in quadrature_table_1D[Nq]:
-        integral += g(row[0]*(b-a)/2 + (b+a) / 2)*row[1]
-    return integral*(b-a) / 2
+        I += g(row[0]*(b-a)/2 + (b+a) / 2)*row[1]
+    return I*(b-a) / 2
 
-for Nq in range(1,5):
-    a = 1
-    b = 2
-    print(quadrature1D(a, b, Nq, np.exp) - np.exp(b) + np.exp(a))
 
+quadrature_table_2D = {
+    1: [[(1/3, 1/3, 1/3) , 1]],
+    3: [[(1/2, 1/2, 0), 1/3 ],
+        [(1/2, 0, 1/2), 1/3],
+        [(0, 1/2, 1/2), 1/3]],
+    4: [[(1/3, 1/3, 1/3), -9/16],
+        [(3/5, 1/5, 1/5), 25/48],
+        [(1/5, 3/5, 1/5), 25/48],
+        [(1/5, 1/5, 3/5), 25/48]]
+}
+
+
+def make_barycentric_to_R2(p1, p2, p3):
+    return lambda z1, z2, z3:  z1*p1 + z2*p2 + z3*p3
+
+def vertices_to_area_2D(p1, p2, p3):
+    return np.abs(0.5*((p2[0]-p1[0])*(p3[1] -p1[1])- (p2[1]-p1[1])*(p3[0]-p1[0])))
+
+def quadrature2D(p1, p2, p3, Nq, g):
+    barycentric_to_R2 = make_barycentric_to_R2(p1, p2, p3)
+    I = 0
+    for row in quadrature_table_2D[Nq]:
+        I += g(barycentric_to_R2(*(row[0])))*row[1]
+    return I*vertices_to_area_2D(p1,p2,p3)
 
---
+++ part1/test_quadrature.py
@@ -0,0 +1,66 @@
+import unittest
+import numpy as np
+from quadrature import quadrature1D, quadrature2D, vertices_to_area_2D
+from integrate_log import get_log_integral
+
+class TestQuadratureMethods(unittest.TestCase):
+    @staticmethod
+    def identity(x):
+        return x
+
+    @staticmethod
+    def get_one_form(z):
+        return lambda x: np.inner(z,x)
+
+    def test_linear_1D(self):
+        for a in range(-10,10):
+            for b in range(a, 10):
+                for Nq in range(1, 5):
+                    self.assertAlmostEqual(quadrature1D(a, b, Nq, self.identity), 0.5 * (b ** 2 - a ** 2))
+
+    def test_exponential_1D(self):
+        a = 1
+        b = 2
+        print("Testing 1D quadrature on exponential function")
+        for Nq in range(1,5):
+            print(f"I_{Nq} error:", quadrature1D(a, b, Nq, np.exp) - np.exp(b) + np.exp(a))
+            self.assertAlmostEqual(quadrature1D(a, b, Nq, np.exp),  np.exp(b) - np.exp(a),delta=1)
+
+    def test_constant_2D(self):
+        p1 = np.array([1, 0])
+        p2 = np.array([3, 1])
+        p3 = np.array([3, 2])
+
+        g = lambda x: 3
+
+        for Nq in [1, 3, 4]:
+            self.assertAlmostEqual(quadrature2D(p1, p2, p3, Nq, g),
+                                   vertices_to_area_2D(p1, p2, p3) * g(0))
+
+    def test_linear_2D(self):
+        for a in range(0, 10):
+            for b in range(0, 10):
+                p1 = np.array([1, 3])
+                p2 = p1 + np.array([0, 1])
+                p3 = p1 + np.array([1, 0])
+                g = self.get_one_form([a,b])
+                min_val = np.min([g(p1), g(p2), g(p3)])
+                diff_val = np.max([g(p1), g(p2), g(p3)]) - min_val
+                for Nq in [1,3,4]:
+                    self.assertAlmostEqual(quadrature2D(p1, p2, p3, Nq, g), ((a+b)/3 + min_val)*vertices_to_area_2D(p1, p2, p3))
+            print("")
+
+    def test_logarithm_2D(self):
+        p1 = np.array([1, 0])
+        p2 = np.array([3, 1])
+        p3 = np.array([3, 2])
+        g  = lambda x: np.log(np.sum(x))
+        I_analytic = get_log_integral()
+        print("Testing 1D quadrature on log function")
+        for Nq in [1, 3, 4]:
+            print(f"I_{Nq} error:", quadrature2D(p1, p2, p3, Nq, g) - I_analytic)
+            self.assertAlmostEqual(quadrature2D(p1, p2, p3, Nq, g),I_analytic, delta=1)
+
+
+if __name__ == '__main__':
+    unittest.main()
