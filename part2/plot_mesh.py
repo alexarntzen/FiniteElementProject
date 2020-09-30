6a0a68aec3cf53bc4189a40905e4430f7992589e
hakonnoren <hakon.noren@gmail.com>
2020-09-30 10:13:28 +0200

    Function for plotting disc mesh generated by getdisc.py

---
+++ part2/plot_mesh.py
@@ -0,0 +1,20 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.collections import LineCollection
+import getdisc as gd
+
+N = 4
+p,tri,edge = gd.GetDisc(N)
+
+def plot_disc(p,tri):
+    line_segments = np.zeros((len(tri),4,2))
+    for i,t in enumerate(tri):
+        for j in range(3):
+            line_segments[i,j,:] = p[t[j]]
+        line_segments[i,3,:] = p[t[0]]
+    
+    plt.scatter(p[:,0],p[:,1])
+    plt.gca().add_collection(LineCollection(seg))
+    plt.show()
+    
+plot_disc(p,tri)
