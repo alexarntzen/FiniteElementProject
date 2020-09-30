332170b35c53ba79586c96e8f75b884fc20f6891
hakonnoren <hakon.noren@gmail.com>
2020-09-30 10:38:15 +0200

    Plotting mesh smarter

--- part2/plot_mesh.py
+++ part2/plot_mesh.py
@@ -3,18 +3,15 @@ import matplotlib.pyplot as plt
 from matplotlib.collections import LineCollection
 import getdisc as gd
 
-N = 200
+N = 100
 p,tri,edge = gd.GetDisc(N)
 
 def plot_disc(p,tri):
-    line_segments = np.zeros((len(tri),4,2))
-    for i,t in enumerate(tri):
-        for j in range(3):
-            line_segments[i,j,:] = p[t[j]]
-        line_segments[i,3,:] = p[t[0]]
-    
+    new_tri = np.concatenate((
+        tri,np.array([tri[:,0].T]).T),axis=1)
     plt.scatter(p[:,0],p[:,1])
-    plt.gca().add_collection(LineCollection(line_segments))
+    plt.gca().add_collection(LineCollection(p[new_tri]))
     plt.show()
     
-plot_disc(p,tri)
+#plot_disc(p,tri)
+
