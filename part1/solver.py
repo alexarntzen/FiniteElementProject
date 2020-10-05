e8c7689cbf560815454d0291e653939de51c9aff
Alexander <alexander.arntzen@gmail.com>
2020-10-05 17:26:05 +0200

    Added final programming task

--- part1/solver.py
+++ part1/solver.py
@@ -1,51 +1,65 @@
 import numpy as np
+from itertools import combinations
 import part1.quadrature as qd
 
-def solve(p,tri,edge,Nq,f):
+
+def compose(f,g):
+    return lambda x: f(g(x))
+
+def function_multiply(f,g):
+    return lambda x: f(x)*g(x)
+
+def solve(p, tri, dirichlet_edges, Nq, f, g = None, neumann_edges = np.empty(0)):
     n_bar = len(p)
-    A = np.zeros((n_bar,n_bar))
+    A = np.zeros((n_bar, n_bar))
     F = np.zeros(n_bar)
-    
-    
+
     for element in tri:
-        
-        #find coefficients for basis functions
-        XY = np.ones((3,3))
-        C = np.zeros((3,3))
-        XY[:,1:] = p[element]
-        
-        for i in range(3):
-            b = np.zeros(3)
-            b[i] = 1
-            C[:,i] = np.linalg.solve(XY,b)
-        
-        #coordinates of the nodes of the element
-        p1,p2,p3 = p[element[0]],p[element[1]],p[element[2]]
-        
-        #find a(phi_i,phi_j) and l(phi_i)
+
+        # find coefficients for basis functions
+
+        XY = np.append(np.ones((3,1)), p[element], axis = 1)
+        C = np.linalg.solve(XY, np.identity(3))
+
+        # coordinates of the nodes of the element
+        p1, p2, p3 = p[element[0]], p[element[1]], p[element[2]]
+
+        # find a(phi_i,phi_j) and l(phi_i)
         for alpha in range(3):
-            
-            #finding F vector
-            fHa = lambda x: (C[0,alpha] + C[1,alpha]*x[0] + C[2,alpha]*x[1])*f(x)
-            F_a = qd.quadrature2D(p1, p2, p3, Nq, fHa)
+
+            # finding F vector
+            Ha = lambda x: (C[0, alpha] + C[1:3, alpha] @ x)
+
+            F_a = qd.quadrature2D(p1, p2, p3, Nq, function_multiply(Ha, f))
             F[element[alpha]] += F_a
-            
-            for beta in range(3): 
-                
-                #finding A matrix
-                HaHb_derivative = lambda x: C[1,alpha]*C[1,beta] + C[2,alpha]*C[2,beta]
+
+            for beta in range(3):
+                # finding A matrix
+
+                HaHb_derivative = lambda x: C[1, alpha] * C[1, beta] + C[2, alpha] * C[2, beta]
                 I_ab = qd.quadrature2D(p1, p2, p3, Nq, HaHb_derivative)
-                A[element[alpha],element[beta]] += I_ab
-                
-    #Applying dirichlet boundary conditions
+                A[element[alpha], element[beta]] += I_ab
+
+                if [element[alpha], element[beta]] in neumann_edges.tolist():
+                    vertex1, vertex2 = p[element[alpha]], p[element[beta]]
+                    Hb = lambda x: (C[0, beta] + C[1:3, beta] @ x)
+
+                    F[element[alpha]] += qd.quadrature1D(vertex1, vertex2, Nq, function_multiply(Ha,g))
+                    F[element[beta]] += qd.quadrature1D(vertex1, vertex2, Nq, function_multiply(Hb,g))
+
+        # for edge in combinations(element,2):
+        #     print(edge)
+        # print("lskjdfasfd")
+
+    # Applying dirichlet boundary conditions
     epsilon = 1e-15
-    for i in edge[:,0]:
-        A[i,i] = 1/epsilon
+    for i in np.concatenate((dirichlet_edges[:, 0],dirichlet_edges[:, 1])):
+        A[i, :] = 0
+        A[:, i] = 0
+        A[i, i] = 1
         F[i] = 0
-    
-    #solving AU = F
-    U = np.linalg.solve(A,F)
-        
+
+    # solving AU = F
+    U = np.linalg.solve(A, F)
+
     return U
-
-
--- test/test_solver.py
+++ test/test_solver.py
@@ -1,7 +1,7 @@
 import unittest
 import numpy as np
 import matplotlib.pyplot as plt
-
+from test.plot_mesh import plot_disc
 from part1.solver import solve
 import test.getdisc as gd
 
@@ -12,7 +12,15 @@ def f(x):
     r2 = x[0]**2 + x[1]**2
     return -8*np.pi*np.cos(2*np.pi*r2) + 16*np.pi**2*r2*np.sin(2*np.pi*r2)
 
+def g(x):
+    return 4*np.pi*np.linalg.norm(x)*np.cos(2*np.pi*np.sum(x**2))
+
 class TestSolver(unittest.TestCase):
+    def test_plot_mesh(self):
+        N = 10
+        p, tri, edge = gd.GetDisc(N)
+        plot_disc(p,tri)
+
     def test_compare_analytic(self):
         test_values = 2**np.arange(2,11)
         for N in test_values:
@@ -29,17 +37,48 @@ class TestSolver(unittest.TestCase):
             self.assertAlmostEqual(max_error,0,delta=1e2/N)
 
     def test_plot(self):
-        N = 300
+        N = 500
         fig = plt.figure()
         ax = fig.gca(projection='3d')
         #ax.plot_trisurf(p[:,0],p[:,1],U)
         p, tri, edge = gd.GetDisc(N)
         U = solve(p, tri, edge, 4, f)
 
-        ax.plot_trisurf(p[:,0],p[:,1],U-u(p.T))
+        ax.plot_trisurf(p[:,0],p[:,1],U)
         plt.show()
 
 
+class TestSolverNeumann(unittest.TestCase):
+    def test_compare_analytic(self):
+        test_values = 2**np.arange(4,12)
+        for N in test_values:
+
+            # p is coordinates of all nodes
+            # tri is a list of indicies (rows in p) of all nodes belonging to one element
+            # edge is lists of all nodes on the edge
+            p, tri, edge = gd.GetDisc(N)
+            neumann_edges = edge[(p[edge[:,0]][:, 1] > 0 ) & (p[edge[:,1]][:, 1] > 0)]
+            dirichlet_edges = edge[(p[edge[:,0]][:, 1] <= 0 ) | (p[edge[:,1]][:, 1] <= 0)]
+
+
+            # analytical solution
+            U = solve(p, tri, dirichlet_edges, 4, f=f, g=g, neumann_edges=neumann_edges)
+            max_error = np.max(np.abs(U-u(p.T)))
+            print(f"N = {N}, max error:", max_error)
+            self.assertAlmostEqual(max_error,0,delta=1e2/N)
+
+    def test_plot(self):
+        N = 500
+        fig = plt.figure()
+        ax = fig.gca(projection='3d')
+        p, tri, edge = gd.GetDisc(N)
+        neumann_edges = edge[(p[edge[:, 0]][:, 1] > 0) & (p[edge[:, 1]][:, 1] > 0)]
+        dirichlet_edges = edge[(p[edge[:, 0]][:, 1] <= 0) | (p[edge[:, 1]][:, 1] <= 0)]
+
+        U = solve(p, tri, dirichlet_edges, 4, f=f, g=g, neumann_edges=neumann_edges)
+
+        ax.plot_trisurf(p[:,0],p[:,1],U)
+        plt.show()
 
 
 
