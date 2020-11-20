350df6dcc4ef2b1690d53e55054bdb096d9802cc
Alexander <alexander.arntzen@gmail.com>
2020-11-20 12:55:08 +0100

    chagned something

--- test/test_elasticity_solver.py
+++ test/test_elasticity_solver.py
@@ -2,10 +2,12 @@ import unittest
 
 import matplotlib.pyplot as plt
 import numpy as np
+import scipy.sparse as sp
+import scipy.sparse.linalg as splin
 from matplotlib import cm
 from mpl_toolkits.mplot3d import Axes3D
 import test.getplate as gp
-from femsolver.elasticity_solver import solve_elastic
+from femsolver.elasticity_solver import solve_elastic, get_elasticity_A_F, reshape_U
 import time
 
 
@@ -30,30 +32,6 @@ def get_C(E, v):
         [v, 1, 0],
         [0, 0, (1 - v) / 2]
     ])
-
-
-def large_to_small_mapping(N_large, N_small):
-    # assuming N = 2**i + 1
-
-    scale = (N_large - 1) // (N_small - 1)
-    l_to_s_1D = np.arange(0, N_small) * scale
-    l_to_s_2D = np.ix_(l_to_s_1D, l_to_s_1D)
-
-    return l_to_s_2D
-
-# get the solution and computaiton time
-def get_solution(N, E, v):
-    # p is coordinates of all nodes
-    # tri is a list of indicies (rows in p) of all nodes belonging to one element
-    # edge is lists of all nodes on the edge
-    p, tri, edge = gp.getPlate(N)
-    edge -= 1
-
-    # Using time.time() to find the runtime of solve_elastic()
-    t1 = time.time()
-    U = solve_elastic(p, tri, edge, C=get_C(E, v), f=get_f(E, v))
-    t2 = time.time()
-    return U, t2 - t1
 
 
 # Test the solver on the problem from task 2
@@ -91,80 +69,10 @@ class TestElasticHomogeneousDirichlet(unittest.TestCase):
         plt.savefig("figures/convergence_homogeneous_dirichlet_elastic.pdf")
         plt.clf()
 
-    # Test the solver for a problem with small element sizes
-    # and compare with solutions on a coarser mesh
-    def test_decreasing_h(self):
-
-        E = 5
-        v = 0.1
-
-        # Maximum N / minimum stepsize, h = 1/(2**i_max)
-        i_max = 6
-        N_finest = 2 ** i_max + 1
-        N_list = 2 ** np.arange(1, i_max - 1) + 1
-
-        test_values = N_list ** 2 * 2
-        u_max = 1
-
-        rel_errors = np.zeros(len(N_list))
-        run_times = np.zeros(len(N_list) + 1)
-
-        print(f"\n\nComparing elastic solution for h_small = {1 / N_finest:.3f} to larger step sizes.")
-
-        U_finest, td = get_solution(N_finest, E, v)
-        run_times[-1] = td
-
-        for i, N in enumerate(N_list):
-            U_N, td = get_solution(N, E, v)
-            u_max = np.max(np.abs(U_N))
-
-            # getting points that have same location in fine and coarse mesh
-            r = (N_finest - 1) // (N - 1)
-            l_to_s_index = np.array([np.arange(0 + j * r * N_finest, N_finest + j * r * N_finest, r).tolist() for j in
-                                     range(0, N)]).flatten()
-            max_error = np.max(np.abs(U_N[:, 0] - U_finest[:, 0][l_to_s_index]))
-            print(f"h = {1 / N:.2f}, max error: {max_error:.5f} run time: {td:.3f}")
-            self.assertAlmostEqual(max_error, 0, delta=10 / test_values[i])
-
-            rel_errors[i] = max_error
-            run_times[i] = td
-
-        N_list = np.append(N_list, N_finest)
-        element_sizes = 1 / N_list
-
-        error_convergence = np.polyfit(np.log(element_sizes[:-1]), np.log(rel_errors), deg=1)[0]
-
-        print("\n\nGenrating plot for solution convergence and time complexity for the linear elasticity problem ")
-
-        plt.gcf().subplots_adjust(left=0.15)
-        #plt.title("Relative deviance for different element sizes")
-        plt.loglog(element_sizes[:-1], rel_errors, marker="o")
-
-        plt.ylabel(" $|| U_f - U_h ||_{\infty}$")
-        plt.xlabel("Element size, $h$")
-        plt.savefig("figures/decreasing_h_error.pdf")
-        plt.clf()
-
-        time_convergence = np.polyfit(np.log(element_sizes), np.log(run_times), deg=1)[0]
-
-        plt.gcf().subplots_adjust(left=0.15)
-        #plt.title("Runtime for different element sizes")
-        plt.loglog(element_sizes, run_times, marker="o")
-        plt.ylabel("Run time ($s$)")
-        plt.xlabel("Element size, $h$")
-        plt.savefig("figures/decreasing_h_runtime.pdf")
-        plt.clf()
-
-        convergence = np.column_stack((error_convergence, time_convergence))
-
-        np.savetxt("figures/convergence_elasticity.dat", convergence, header="error_convergence \t time_convergence", fmt='%.2f',
-                   delimiter='\t')
-
     def test_plot(self):
         N = 32
         E = 200
         v = 0.1
-
 
         p, tri, edge = gp.getPlate(N)
         edge -= 1  # The edge indexes seem to be off
@@ -211,6 +119,152 @@ class TestElasticHomogeneousDirichlet(unittest.TestCase):
         # ax.plot_trisurf(p[:, 0], p[:, 1], U)
 
         plt.savefig("figures/plot_homogeneous_dirichlet_elastic_y.pdf")
+        plt.clf()
+
+
+def large_to_small_mapping(N_large, N_small):
+    # assuming N = 2**i + 1
+
+    scale = (N_large - 1) // (N_small - 1)
+    l_to_s_1D = np.arange(0, N_small) * scale
+    l_to_s_2D = np.ix_(l_to_s_1D, l_to_s_1D)
+
+    return l_to_s_2D
+
+
+# Functions for timing of solution time
+def solve_LU(A, F):
+    U = np.linalg.solve(A, F)
+    return reshape_U(U)
+
+
+def solve_sparse(A, F):
+    A_sp = sp.csr_matrix(A)
+    U = splin.spsolve(A_sp, F)
+    return reshape_U(U)
+
+
+def solve_cg(A, F):
+    A_sp = sp.csr_matrix(A)
+    U = splin.cg(A_sp, F, atol=0.1)[0]
+    return reshape_U(U)
+
+
+# get the solution and computaiton time
+def get_solution(N, E, v):
+    # p is coordinates of all nodes
+    # tri is a list of indicies (rows in p) of all nodes belonging to one element
+    # edge is lists of all nodes on the edge
+    p, tri, edge = gp.getPlate(N)
+    edge -= 1
+
+    # Using time.time() to find the runtime of solve_elastic()
+    t1 = time.time()
+    A, F = get_elasticity_A_F(p, tri, edge, C=get_C(E, v), f=get_f(E, v))
+    t2 = time.time()
+    make_time = t2 - t1
+
+    t1 = time.time()
+    U = solve_LU(A, F)
+    t2 = time.time()
+    lu_time = t2 - t1
+
+    t1 = time.time()
+    U = solve_sparse(A, F)
+    t2 = time.time()
+    sparse_time = t2 - t1
+
+    t1 = time.time()
+    U = solve_cg(A, F)
+    t2 = time.time()
+    cg_time = t2 - t1
+
+    return U, make_time, lu_time, sparse_time, cg_time
+
+
+class TestElasticSolverPerformance(unittest.TestCase):
+
+    # Test the solver for a problem with small element sizes
+    # and compare with solutions on a coarser mesh
+    def test_decreasing_h(self):
+        E = 5
+        v = 0.1
+
+        # Maximum N / minimum stepsize, h = 1/(2**i_max)
+        i_max = 6
+        N_finest = 2 ** i_max + 1
+        N_list = 2 ** np.arange(1, i_max - 1) + 1
+
+        test_values = N_list ** 2 * 2
+        u_max = 1
+
+        rel_errors = np.zeros(len(N_list))
+        make_times = np.zeros(len(N_list) + 1)
+        lu_times = np.zeros(len(N_list) + 1)
+        sparse_times = np.zeros(len(N_list) + 1)
+        cg_times = np.zeros(len(N_list) + 1)
+
+        print(f"\n\nComparing elastic solution for h_small = {1 / N_finest:.3f} to larger step sizes.")
+
+        U_finest, make_time, lu_time, sparse_time, cg_time = get_solution(N_finest, E, v)
+        make_times[-1] = make_time
+        lu_times[-1] = lu_time
+        sparse_times[-1] = sparse_time
+        cg_times[-1] = cg_time
+
+        for i, N in enumerate(N_list):
+            U_N, make_time, lu_time, sparse_time, cg_time = get_solution(N, E, v)
+            u_max = np.max(np.abs(U_N))
+
+            # getting points that have same location in fine and coarse mesh
+            r = (N_finest - 1) // (N - 1)
+            l_to_s_index = np.array([np.arange(0 + j * r * N_finest, N_finest + j * r * N_finest, r).tolist() for j in
+                                     range(0, N)]).flatten()
+            max_error = np.max(np.abs(U_N[:, 0] - U_finest[:, 0][l_to_s_index]))
+            print(f"h = {1 / N:.2f}, max error: {max_error:.5f} run time: {sum([make_time + sparse_time]):.3f}")
+            self.assertAlmostEqual(max_error, 0, delta=10 / test_values[i])
+
+            rel_errors[i] = max_error
+            make_times[i] = make_time
+            lu_times[i] = lu_time
+            sparse_times[i] = sparse_time
+            cg_times[i] = cg_time
+
+        N_list = np.append(N_list, N_finest)
+        element_sizes = 1 / N_list
+
+        error_convergence = np.polyfit(np.log(element_sizes[:-1]), np.log(rel_errors), deg=1)[0]
+
+        print("\n\nGenerating plot for solution convergence and time complexity for the linear elasticity problem ")
+
+        plt.gcf().subplots_adjust(left=0.15)
+        # plt.title("Relative deviance for different element sizes")
+        plt.loglog(element_sizes[:-1], rel_errors, marker="o")
+
+        plt.ylabel(" $|| U_f - U_h ||_{\infty}$")
+        plt.xlabel("Element size, $h$")
+        plt.savefig("figures/decreasing_h_error.pdf")
+        plt.clf()
+
+        time_convergence_make = np.polyfit(np.log(element_sizes), np.log(make_times), deg=1)[0]
+        time_convergence_LU = np.polyfit(np.log(element_sizes), np.log(lu_times), deg=1)[0]
+        time_convergence_sparse = np.polyfit(np.log(element_sizes), np.log(sparse_times), deg=1)[0]
+        time_convergence_cg = np.polyfit(np.log(element_sizes), np.log(cg_times), deg=1)[0]
+        print(f"\nEstimated time complexity for make system: O(h^{time_convergence_make}) ")
+        print(f"Estimated time complexity of solve LU: O(h^{time_convergence_LU}) ")
+        print(f"Estimated time complexity of solve sparse: O(h^{time_convergence_sparse}) ")
+        print(f"Estimated time complexity of of solve conjugate gradient: O(h^{time_convergence_cg}) ")
+
+        plt.gcf().subplots_adjust(left=0.15)
+        # plt.title("Runtime for different element sizes")
+        plt.loglog(element_sizes, make_times, marker="o", label="make system")
+        plt.loglog(element_sizes, lu_times, marker="o", label="solve LU")
+        plt.loglog(element_sizes, sparse_times, marker="o", label="solve sparse")
+        plt.loglog(element_sizes, cg_times, marker="o", label="solve conjugate gradient")
+        plt.legend()
+        plt.ylabel("Run time ($s$)")
+        plt.xlabel("Element size, $h$")
+        plt.savefig("figures/decreasing_h_runtime.pdf")
         plt.clf()
 
 
